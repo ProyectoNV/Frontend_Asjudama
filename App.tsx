8a0e516911876ea67c84a60cb3672385d740aa12
Daniel Español <danielespanol494@gmail.com>
2024-05-23 08:23:48 -0500

    Se sube interfaz de registro productos y correxión en login

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { LoginScreen } from './src/Views/Login';
-
+import LoginScreen from './src/Views/Login';
+import RegisterProduct from './src/Views/Registro_producto';
 
 export type RootStackParamList = {
-  LoginScreen: {estado:boolean};
+  LoginScreen: { estado: boolean };
+  RegisterProduct: { estado: boolean };
+  // Agrega más pantallas aquí según sea necesario
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -15,7 +17,6 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          headerShown: true,
           headerStyle: {
             backgroundColor: '#05bcc1', 
           },
@@ -24,20 +25,25 @@ const App = () => {
             fontWeight: 'bold', 
           },
         }}>
-          <Stack.Screen
+        <Stack.Screen
           name="LoginScreen"
           component={LoginScreen}
           options={{
-            title: 'Actividades',
+            title: 'Login',
+            headerShown: false,  // Oculta la barra de navegación en la pantalla de inicio de sesión
           }}
-          initialParams={{estado:false}}
-          />
+          initialParams={{ estado: false }}
+        />
+        <Stack.Screen
+          name="RegisterProduct"
+          component={RegisterProduct}
+          options={{
+            title: 'Registrar Productos',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
-
-
-
--- src/Views/Registro_producto.tsx
+++ src/Views/Registro_producto.tsx
@@ -1,49 +1,60 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView, Alert } from 'react-native';
-import { useNavigation, NavigationProp } from '@react-navigation/native';
-import { RootStackParamList } from '../../App'; // Asegúrate de importar correctamente
 
-const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+const RegisterProduct = () => {
+  const [nameproduct, setNameproduct] = useState('');
+  const [precio, setPrecio] = useState('');
+  const [descripción, setDireccion] = useState('');
 
   const handleRegister = () => {
-    if (!email || !password) {
+    if (!nameproduct || !precio || !descripción) {
       Alert.alert('Error', 'Por favor completa todos los campos obligatorios.');
+      return;
+    }
+
+    const nameRegex = /^[a-zA-Z\s]+$/;
+    if (!nameRegex.test(nameproduct)) {
+      Alert.alert('Error', 'Por favor ingresa un nombre válido (solo letras).');
+      return;
+    }
+
+
+    const numDocRegex = /^[0-9]+$/;
+    if (!numDocRegex.test(precio)) {
+      Alert.alert('Error', 'Por favor ingresa un precio válido (solo números).');
       return;
     }
 
     // Lógica de registro
     Alert.alert('¡Éxito!', '¡Cliente registrado correctamente!');
-    console.log('Registrarse:', { email, password });
-    
-    // Navegar a RegisterProduct
-    navigation.navigate('RegisterProduct', { estado: true });
+    console.log('Registrarse:', { nameproduct, precio, descripción });
   };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>Ingreso</Text>
+      <Text style={styles.title}>Regístra un producto</Text>
       <View style={styles.formContainer}>
         <TextInput
           style={styles.input}
-          placeholder="Correo electrónico"
-          onChangeText={text => setEmail(text)}
-          value={email}
-          keyboardType="email-address"
+          placeholder="Nombre"
+          onChangeText={text => setNameproduct(text)}
+          value={nameproduct}
         />
         <TextInput
           style={styles.input}
-          placeholder="Contraseña"
-          onChangeText={text => setPassword(text)}
-          value={password}
-          secureTextEntry
+          placeholder="Valor del producto"
+          onChangeText={text => setPrecio(text)}
+          value={precio}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Descripción"
+          onChangeText={text => setDireccion(text)}
+          value={descripción}
         />
       </View>
       <TouchableOpacity style={styles.button} onPress={handleRegister}>
-        <Text style={styles.buttonText}>Iniciar Sesión</Text>
+        <Text style={styles.buttonText}>Registrar</Text>
       </TouchableOpacity>
     </ScrollView>
   );
@@ -95,4 +106,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default LoginScreen;
+export default RegisterProduct
