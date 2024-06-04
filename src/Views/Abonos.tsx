a8a450fc33d8d1826ef5e594875f44ec5ab490b9
PipelonE <danielespanol494@gmail.com>
2024-06-03 21:52:40 -0500

    Se suben vistas de registro de abonos en facturas

--- src/Views/Abonos.tsx
+++ src/Views/Abonos.tsx
@@ -1,96 +1,140 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView, Alert } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
-import { RootStackParamList } from '../../App'; // Asegúrate de importar correctamente
+import { RootStackParamList } from '../../App';
 
 const RegisterAbono = () => {
-  const [amount, setAmount] = useState('');
-  const [description, setDescription] = useState('');
+    const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+    const [abonos, setAbonos] = useState([]);
+    const [idclient, setIdclient] = useState("");
+    const [tdoc, setTdoc] = useState(""); // Nuevo estado para el tipo de documento
+    const [vendedorid, setVendedorid] = useState("2"); // ID del vendedor (puede ajustarse según sea necesario)
 
-  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+    const handleBuscar = async () => {
+        if (idclient && tdoc) { // Solo ejecutar si ambos valores están presentes
+            try {
+                const response = await fetch(`http://192.168.209.37:4000/vendedor/buscar_factura_cliente_vendedor/${vendedorid}/${tdoc}/${idclient}`);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const databonos = await response.json(); // Parsear directamente como JSON
+                setAbonos(databonos);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    };
 
-  const handleRegister = () => {
-    if (!amount || !description) {
-      Alert.alert('Error', 'Por favor completa todos los campos obligatorios.');
-      return;
-    }
+    useEffect(() => {
+        handleBuscar();
+    }, [idclient, tdoc, vendedorid]); // Ejecutar useEffect cuando cambien estos valores
 
-    const amountRegex = /^[0-9]+$/;
-    if (!amountRegex.test(amount)) {
-      Alert.alert('Error', 'Por favor ingresa un monto válido (solo números).');
-      return;
-    }
+    const formatDate = (dateString: string) => {
+      const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
+      return new Date(dateString).toLocaleDateString(undefined, options);
+    };
 
-    // Lógica de registro
-    Alert.alert('¡Éxito!', '¡Abono registrado correctamente!');
-    console.log('Registrar abono:', { amount, description });
-  };
-
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>Registrar Abono</Text>
-      <View style={styles.formContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Monto"
-          onChangeText={text => setAmount(text)}
-          value={amount}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Descripción"
-          onChangeText={text => setDescription(text)}
-          value={description}
-        />
-      </View>
-      <TouchableOpacity style={styles.button} onPress={handleRegister}>
-        <Text style={styles.buttonText}>Registrar</Text>
-      </TouchableOpacity>
-    </ScrollView>
-  );
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <Text style={styles.title}>Facturas Pendientes</Text>
+                <View style={styles.buscar}>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={text => setTdoc(text)}
+                        value={tdoc}
+                        placeholder="Ingrese tipo de documento del cliente"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={text => setIdclient(text)}
+                        value={idclient}
+                        placeholder="Ingrese número de documento del cliente"
+                    />
+                    <TouchableOpacity style={styles.buscarIcon} onPress={handleBuscar}>
+                        <Image source={require('../../imagenes/Buscar.png')} style={styles.buscarImage} />
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    {abonos.map((abon: { numero_factura_venta: number; fecha_factura: string; total_factura: number; estado_factura: number }) => (
+                        <View key={abon.numero_factura_venta} style={styles.zoneContainer}>
+                            <Text style={styles.zoneText}>Numero: {abon.numero_factura_venta}</Text>
+                            <Text style={styles.zoneText}>Valor: {abon.total_factura}</Text>
+                            <Text style={styles.zoneText}>Fecha: {formatDate(abon.fecha_factura)}</Text>
+                            <Text style={styles.zoneText}>Estado: Pendiente</Text>
+                            <TouchableOpacity onPress={() => navigation.navigate('AgregarAbono', { id_factu: abon.numero_factura_venta })} style={styles.deleteButton}>
+                                <Text style={styles.deleteButtonText}>Hacer Abono</Text>
+                            </TouchableOpacity>
+                        </View>
+                    ))}
+                </View>
+            </View>
+        </ScrollView>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    backgroundColor: '#fdf3ec',
-    paddingHorizontal: 30,
-    paddingTop: 50,
-  },
-  title: {
-    fontSize: 36,
-    fontWeight: 'bold',
-    marginBottom: 30,
-    color: '#85C1E9',
-  },
-  formContainer: {
-    marginBottom: 30,
-  },
-  input: {
-    width: '100%',
-    height: 50,
-    backgroundColor: '#FFFFFF',
-    borderWidth: 1,
-    borderColor: '#D3D3D3',
-    borderRadius: 10,
-    paddingHorizontal: 20,
-    marginBottom: 20,
-    fontSize: 18,
-    color: '#333',
-  },
-  button: {
-    width: '100%',
-    height: 60,
-    backgroundColor: '#FF6347',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: 'bold',
-  },
+    container: {
+        flexGrow: 1,
+        backgroundColor: '#fdf3ec',
+        paddingHorizontal: 30,
+        paddingTop: 50,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        textAlign: 'center',
+        color: '#85C1E9',
+    },
+    zoneContainer: {
+        backgroundColor: '#f5f5f5',
+        padding: 15,
+        marginBottom: 15,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#D3D3D3',
+    },
+    zoneText: {
+        fontSize: 18,
+        color: '#333',
+    },
+    deleteButton: {
+        backgroundColor: '#FF6347',
+        marginTop: 10,
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+    deleteButtonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    buscar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+        paddingHorizontal: 20,
+    },
+    buscarIcon: {
+        marginLeft: 10,
+        padding: 10,
+    },
+    buscarImage: {
+        width: 20,
+        height: 20,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        marginBottom: 10,
+        backgroundColor: '#fff',
+        flex: 1,
+        marginRight: 10, // Para dar espacio entre los inputs
+    },
 });
 
 export default RegisterAbono;
